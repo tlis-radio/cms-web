1ca0e1b2ba758d554e148f916daefd90c0433230
NAN0 <135762196+ONAN0@users.noreply.github.com>
2024-06-15 02:36:25 +0200

    social media added

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,23 +1,23 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-//import {} from "@fortawesome/free-solid-svg-icons";
+import {faFacebook, faInstagram, faSpotify, faYoutube} from "@fortawesome/free-brands-svg-icons";
 
 const Navbar = () => {
    return (
-      <nav className='bg-cyan-500 flex flex-row justify-center h-full w-full p-4'>
+      <nav className='bg-cyan-500 flex flex-row justify-between h-full w-full p-4'>
          <div className='bg-black flex p-1 text-white'>TLIS</div>
          Navbar
-         <div className='bg-black flex p-1'>
-            <a className='flex w-5 text-white border rounded-xl' href="https://www.facebook.com/radiotlis/">
-            <FontAwesomeIcon icon={'facebook-f'} />
+         <div className='bg-black flex p-1 gap-3'>
+            <a className='flex w-8 text-white border rounded-3xl p-1' href="https://www.facebook.com/radiotlis/">
+            <FontAwesomeIcon icon={faFacebook} />
             </a>
-            <a className='flex w-5 text-white border rounded-xl' href="https://www.instagram.com/radiotlis/">
-            <FontAwesomeIcon icon={'instagram'} />
+            <a className='flex w-8 text-white border rounded-3xl p-1' href="https://www.instagram.com/radiotlis/">
+            <FontAwesomeIcon icon={faInstagram} />
             </a>
-            <a className='flex w-5 text-white border rounded-xl' href="https://open.spotify.com/user/wskoqdo1mcisldwdvmzgj8bvf?si=e6b0940811194b4e">
-            <FontAwesomeIcon icon={'spotify'} />
+            <a className='flex w-8 text-white border rounded-3xl p-1' href="https://open.spotify.com/user/wskoqdo1mcisldwdvmzgj8bvf?si=e6b0940811194b4e">
+            <FontAwesomeIcon icon={faSpotify} />
             </a>
-            <a className='flex w-5 text-white border rounded-xl' href="https://www.youtube.com/@radiotlis">
-            <FontAwesomeIcon icon={'youtube'} />
+            <a className='flex w-8 text-white border rounded-3xl p-1' href="https://www.youtube.com/@radiotlis">
+            <FontAwesomeIcon icon={faYoutube} />
             </a>
          </div>
       </nav>
