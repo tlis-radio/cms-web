2d3abad9c14bb373dd93067e5f6bebdcfa9af596
NAN0 <135762196+ONAN0@users.noreply.github.com>
2024-06-26 22:39:52 +0200

    carousel blur + scale + fade in + fade out

--- src/app/pages/layout.tsx
+++ src/app/pages/layout.tsx
@@ -6,7 +6,7 @@ const PageLayout = ({ children, }: { children: React.ReactNode }) => {
       <>
          <Header />
          <div className='bg-[#00ff00] flex flex-row justify-center w-full'>
-            <div className='bg-[#ff0000] p-4 w-3/4 text-center'>
+            <div className='bg-[#ff0000] pt-4 w-2/3 text-center'>
                {children}
             </div>
          </div>
