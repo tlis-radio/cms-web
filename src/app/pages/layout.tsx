bd7d58ce95c9c2bf5d272cf9760c696e11a0ce3d
Dominik Tulach <60275270+d0m0s159@users.noreply.github.com>
2024-07-14 14:51:07 +0200

    Updated breakpoint in pages/layout.tsx

--- src/app/pages/layout.tsx
+++ src/app/pages/layout.tsx
@@ -17,7 +17,7 @@ const PageLayout = ({ children, }: { children: React.ReactNode }) => {
       <>
          <Header />
          <div className='bg-[#00ff00] flex flex-row justify-center w-full'>
-            <div className='bg-[#ff0000] pt-4 w-full text-center sm:w-3/4 md:w-2/3 xxl:w-1/2'>
+            <div className='bg-[#ff0000] pt-4 w-full text-center sm:w-3/4 md:w-2/3 2xl:w-1/2'>
                {children}
             </div>
          </div>
