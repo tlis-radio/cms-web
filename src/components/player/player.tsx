74aac4fdaa3af34c36c3bb6da86808ecad5dcdef
Dominik Tulach <60275270+d0m0s159@users.noreply.github.com>
2024-07-15 01:18:18 +0200

    Downloaded react-fast-marquee and used it to create marquee text

--- src/components/player/player.tsx
+++ src/components/player/player.tsx
@@ -20,7 +20,7 @@ const Player: React.FC<{ isVisible: boolean }> = ({ isVisible }) => {
    return (
       <>
       <div
-         className="flex items-center bg-[#2e2b2c] p-2 pr-7 text-white fixed bottom-0 inset-x-0 w-full gap-2 z-10
+         className="flex items-center bg-[#2e2b2c] p-2 pr-11 text-white fixed bottom-0 inset-x-0 w-full gap-2 z-10
              sm:w-1/2 sm:pr-0 sm:static 
              xl:w-2/5 2xl:w-1/3"
          style={playerStyle}
--- src/components/player/song-data.tsx
+++ src/components/player/song-data.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Marquee from "react-fast-marquee";
 
 type PlayerDisplayProps = {
    title?: string;
@@ -8,14 +9,18 @@ const PlayerDisplay: React.FC<PlayerDisplayProps> = ({ title }) => {
    return (
       <>
          <span className="px-2" data-tip={title}>
-            <p className="line-clamp-1">{title} {title} {title}</p>
+            <Marquee autoFill direction="right">
+               <p className="line-clamp-1">{title} {title} {title}_</p>
+            </Marquee>
          </span>
          {
          // TODO - Make this text scrolling when longer than the container it's located in
-         // The following and previous code is a placeholder for the marquee animation which we might or might not implement depending on the complexity, line-clamp-1 forces text to only be displayed on one line. -Jager 15.7.2024 0:33
+         // Found a Marquee react library, working but need to fix the whitespace error being displayed
          }
          <span className="px-2">
-            <p className="line-clamp-1">Luca-Dante Spadafora feat. Pingu & Mozart</p>
+            <Marquee autoFill direction="right">
+               <p className="line-clamp-1">Luca-Dante Spadafora feat. Pingu & Mozart_</p>
+            </Marquee>
          </span>
       </>
    );
