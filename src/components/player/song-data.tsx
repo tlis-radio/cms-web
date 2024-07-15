c0e16a0af94924727cb6880d3101486181f5d799
Dominik Tulach <60275270+d0m0s159@users.noreply.github.com>
2024-07-15 14:23:48 +0200

    Revert to (...) solution

--- src/components/player/song-data.tsx
+++ src/components/player/song-data.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Marquee from "react-fast-marquee";
 
 type PlayerDisplayProps = {
    title?: string;
@@ -9,18 +8,14 @@ const PlayerDisplay: React.FC<PlayerDisplayProps> = ({ title }) => {
    return (
       <>
          <span className="px-2" data-tip={title}>
-            <Marquee autoFill direction="right">
                <p className="line-clamp-1">{title}</p>
-            </Marquee>
          </span>
          {
          // TODO - Only use Marquee when the text overflows, some conditional rendering needed
          // Nech sa páči, darček na dobrú noc :D -Jager 1:22 15.7.2024
          }
          <span className="px-2">
-            <Marquee autoFill direction="right">
-               <p className="line-clamp-1">Luca-Dante Spadafora feat. Pingu & Mozart</p>
-            </Marquee>
+            <p className="line-clamp-1">Luca-Dante Spadafora feat. Pingu & Mozart</p>
          </span>
       </>
    );
