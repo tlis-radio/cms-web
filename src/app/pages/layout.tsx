84b6e8237b0f597adbcfb3ed571e7cf16fad490c
NAN0 <135762196+ONAN0@users.noreply.github.com>
2024-07-28 11:54:35 +0200

    final bg edition

--- src/app/pages/layout.tsx
+++ src/app/pages/layout.tsx
@@ -31,34 +31,3 @@ const PageLayout = ({ children, }: { children: React.ReactNode }) => {
 };
 
 export default PageLayout;
-
-/* -------------------------------------------------------------------------- */
-/*                                 2. version                                 */
-/* -------------------------------------------------------------------------- */
-
-/**
-'use client'; 
-
-import Header from '@/components/header'
-import Footer from '@/components/footer'
-
-const PageLayout = ({ children, }: { children: React.ReactNode }) => {
-   return (
-      <div className='bg-acoustic-foam-round min-h-[100vh] flex flex-col bg-repeat-y bg-center bg-contain'>
-         <Header />
-         <div className='flex flex-row justify-center w-full'>
-            {
-               // bg-[#00ff00] - green
-               // bg-[#ff0000] - red
-            }
-            <div className='pt-4 w-full text-center md:w-3/4 xl:w-2/3 2xl:w-1/2'>
-               {children}
-            </div>
-         </div>
-         <Footer />
-      </div>
-   );
-};
-
-export default PageLayout;
-*/
