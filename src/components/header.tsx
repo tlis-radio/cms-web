d0989b4a7c0d00b6d92ca7211180eb4b3284d6d7
d0m0s159 <dominik.tulach@gmail.com>
2024-09-11 19:23:10 +0200

    added-redirect-to-home-from-logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,17 +3,20 @@ import Image from "next/image";
 import Socials from "./socials";
 import logo from "@/../public/images/03_TLIS_logo2020_white_no-bkg.svg";
 import Navbar from "./navbar";
+import Link from "next/link";
 
 const Header = () => {
    return (
       <header className='bg-[#96120F] text-white pr-4 fixed w-full'>
          <div className='flex flex-row justify-between h-[80px] w-full'>
+            <Link href={"/"}>
             <Image
                src={logo}
                alt="Logo"
                height={80}
                priority={true}
             />
+            </Link>
             <PlayerComponent />
             <Socials />
          </div>
--- src/components/navbar/navbar-link.tsx
+++ src/components/navbar/navbar-link.tsx
@@ -7,6 +7,7 @@ type NavbarLinkProps = {
 }
 
 const NavbarLink: FunctionComponent<NavbarLinkProps> = ({ text, redirectUrl }) => {
+   // TODO: "hidden lg:block" into className to hide navbar on mobile devices after hamburger menu is done 
    return (
       <Link href={redirectUrl} className="hover:text-[#96120F] hover:bg-white px-2 transition-colors">
          {text}
