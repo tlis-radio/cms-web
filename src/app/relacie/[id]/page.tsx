3f289b6a41825702a03d3a6b65488e2fab14254a
d0m0s159 <dominik.tulach@gmail.com>
2024-09-11 19:05:36 +0200

    css-bug-fix-in-[id]/page

--- src/app/relacie/[id]/page.tsx
+++ src/app/relacie/[id]/page.tsx
@@ -6,12 +6,12 @@ const Show: React.FC = async ({ params }: any) => {
 
    return (
       <div className="mb-[80px] flex w-full justify-center md:mb-0">
-         <div className="w-full px-2 sm:w-2/3 sm:p-0 md:w-2/3 xl:w-2/5">
+         <div className="w-full -z-10 px-2">
             <div className="flex flex-col gap-4 border bg-[#1c1c1c] p-4 text-white drop-shadow-lg">
                <div className="border-b pb-4">
                   <div className="flex flex-col gap-6 md:flex-row ">
                      <img className="md:h-52" src={show.profileImage.url} />
-                     <div className="mt-auto flex h-max flex-col gap-4">
+                     <div className="m-auto flex h-max w-max flex-col gap-4">
                         <p className="text-2xl font-semibold">{show.name}</p>
                         <p>{show.moderators.map((moderator) => moderator.nickname).join(", ")}</p>
                         <p>Archív: 0</p>
