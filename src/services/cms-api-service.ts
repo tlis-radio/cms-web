108ca4cbe1a3d54f7dd1a145348b91765079bd07
Tomáš Kordoš <kordo.t12@gmail.com>
2025-04-28 18:43:11 +0200

    refactor, clear file comments

--- src/services/cms-api-service.ts
+++ src/services/cms-api-service.ts
@@ -5,59 +5,18 @@ import { Moderator } from "@/models/moderator";
 import { EpisodeDto } from '@/types/episode';
 import { Episode } from '@/models/episode';
 
-import { createDirectus, Query, readItem, readItems, rest } from '@directus/sdk';
+import { createDirectus, readItem, readItems, rest } from '@directus/sdk';
 
 const directus = createDirectus('http://directus.tlis.sk').with(rest());
 
-/*const getData = async <T>(uri: string): Promise<T> => {
-   const response = await fetch(uri);
-
-   if (response.status >= 400) {
-      throw new Error(response.statusText);
-   }
-
-   return response.json();
-}*/
-
 const showEndpoints = {
-   /* UROBIT TYP TOMUTO nech to nepromisuje any */
    listShows: async (): Promise<Array<Show>> => {
       const shows = await directus.request<Array<ShowDto>>(readItems("Shows"));
-      /*const moderators = await directus.request<Array<ModeratorDto>>(readItems("Moderators"));
-      const shows = data.map((show: Show) => {
-         const showModerators = show.Moderators.map((moderator: string) => {
-            const foundModerator = moderators.find((m: Moderator) => m.id === moderator);
-            
-            //console.log(foundModerator);
-
-            return foundModerator ? foundModerator.Name : null;
-         }).filter((name: string | null) => name !== null);
-         
-         //console.log(showModerators);
-
-         return {
-            ...show,
-            Moderators: showModerators,
-         };
-      });*/
-
-      //console.log(shows);
-      
       return shows || [];
    },
 
    getShowDataById: async (id: number): Promise<Show> => {
       const data = await directus.request<ShowDto>(readItem("Shows", id));
-      /*const moderators = await directus.request<Array<ModeratorDto>>(readItems("Moderators"));
-      const showModerators = data.Moderators.map((moderator: string) => {
-         const foundModerator = moderators.find((m: Moderator) => m.id === moderator);
-         return foundModerator ? foundModerator.Name : null;
-      }).filter((name: string | null) => name !== null);
-
-      data.Moderators = showModerators;
-
-      //console.log(data);
-      */
       return data;
    },
 
@@ -67,8 +26,6 @@ const showEndpoints = {
          filter: { id: { _in: showData.Episode } },
          sort: ['id'],
       }));
-
-      //console.log(episodeData);
 
       return episodeData || [];
    },
@@ -81,8 +38,6 @@ const showEndpoints = {
       const moderatorNames = moderatorData.map((moderator: Moderator) => {
          return moderator.Name;
       });
-
-      //console.log("Moderators:", moderatorNames);
 
       return moderatorNames || "";
    }
