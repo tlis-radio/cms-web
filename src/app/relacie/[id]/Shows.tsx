f9c1a16f52b8c18f363aab93f979277c20841d0e
Tomáš Kordoš <kordo.t12@gmail.com>
2025-08-22 19:23:30 +0200

    show whole description

--- src/app/relacie/[id]/Shows.tsx
+++ src/app/relacie/[id]/Shows.tsx
@@ -3,9 +3,26 @@
 import { usePlayer } from "@/context/PlayerContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay } from '@fortawesome/free-solid-svg-icons';
+import { useEffect, useRef, useState } from "react";
+import classNames from "classnames";
 
-export default function Shows({ show, moderators, episodes, ShowName }: { show: any, moderators: Array<string>, episodes: any, ShowName: string }) {
+function Episode({ episode, ShowName }: { episode: any, ShowName: string }) {
     const { setMode, setArchiveName, setSrc, setArchiveEpisodeId, setArchiveMetadata } = usePlayer();
+
+    const [isDescriptionExpanded, setDescriptionExpanded] = useState(false);
+    const [isDescriptionOverflowing, setIsDescriptionOverflowing] = useState(false);
+    const descriptionRef = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+        function checkOverflow() {
+            if (descriptionRef.current) {
+                setIsDescriptionOverflowing(descriptionRef.current.scrollHeight > descriptionRef.current.clientHeight);
+            }
+        }
+
+        checkOverflow();
+        window.addEventListener("resize", checkOverflow);
+        return () => window.removeEventListener("resize", checkOverflow);
+    }, []);
 
     function selectEpisode(episode: any) {
         setMode("archive");
@@ -19,6 +36,68 @@ export default function Shows({ show, moderators, episodes, ShowName }: { show: 
         setArchiveEpisodeId(episode.id);
     }
 
+    return <div className="border bg-[#1c1c1c] p-4 text-white drop-shadow-lg">
+        <div className="flex flex-col md:flex-row gap-4">
+            <img
+                className="w-full h-auto max-h-[200px] md:w-48 md:flex-shrink-0 object-contain"
+                src={`${process.env.NEXT_PUBLIC_DIRECTUS_URL}/assets/` + episode.Cover}
+                alt={episode.Title}
+            />
+
+            <div className="flex-1 flex flex-col">
+                <div className="flex justify-between items-start gap-4">
+                    <div className="flex flex-col items-start">
+                        <h2 className="text-2xl font-semibold flex-1 text-left">{episode.Title}</h2>
+                        <p>{episode.Views} {episode.Views == 1 ? "vypočutie" : "vypočutí"}</p>
+                    </div>
+                    {episode.Audio && episode.Audio !== "" && episode.Audio !== null && (
+                        <button
+                            onClick={() => {
+                                selectEpisode(episode);
+                            }}
+                            className="flex-shrink-0 w-12 h-12 flex items-center justify-center rounded-full bg-[#d43c4a] hover:bg-[#b83744] transition-colors"
+                            aria-label="Play episode"
+                        >
+                            <FontAwesomeIcon icon={faPlay} className="ml-1" />
+                        </button>)}
+                </div>
+
+                <div className={classNames("relative", isDescriptionExpanded ? "pb-10" : "")}>
+                    <div
+                        ref={descriptionRef}
+                        className={`mt-4 ${isDescriptionExpanded ? "max-h-none" : "max-h-32"} overflow-hidden`}
+                    >
+                        <p className="text-justify">
+                            {episode.Description.split("\n").map((line: string, idx: number) => (
+                                <span key={idx}>
+                                    {line}
+                                    <br />
+                                </span>
+                            ))}
+                        </p>
+                    </div>
+
+                    {isDescriptionOverflowing && (
+                        <div className="absolute w-full bottom-0">
+                            {!isDescriptionExpanded && (
+                                <div className="w-full absolute bottom-0 h-20 bg-gradient-to-b from-transparent to-[#1C1C1C]" />
+                            )}
+                            <button
+                                onClick={() => setDescriptionExpanded(!isDescriptionExpanded)}
+                                className={"mt-2 hover:underline absolute bottom-0 -translate-x-1/2 text-[#D43C4A]"}
+                            >
+                                {isDescriptionExpanded ? "Skryť" : "Zobraziť viac"}
+                            </button>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
+    </div>
+
+}
+
+export default function Shows({ show, moderators, episodes, ShowName }: { show: any, moderators: Array<string>, episodes: any, ShowName: string }) {
     return (
         <div className="mb-[80px] flex w-full justify-center md:mb-0">
             <div className="w-full z-10 px-2">
@@ -40,38 +119,7 @@ export default function Shows({ show, moderators, episodes, ShowName }: { show: 
 
                 <div className="mb-8">
                     {episodes.map((episode: any, index: number) => (
-                        <div key={index} className="border bg-[#1c1c1c] p-4 text-white drop-shadow-lg">
-                            <div className="flex flex-col md:flex-row gap-4">
-                                <img
-                                    className="w-full h-auto max-h-52 object-cover md:w-48 md:flex-shrink-0"
-                                    src={`${process.env.NEXT_PUBLIC_DIRECTUS_URL}/assets/` + episode.Cover}
-                                    alt={episode.Title}
-                                />
-
-                                <div className="flex-1 flex flex-col">
-                                    <div className="flex justify-between items-start gap-4">
-                                        <div className="flex flex-col items-start">
-                                            <h2 className="text-2xl font-semibold flex-1 text-left">{episode.Title}</h2>
-                                            <p>{episode.Views} {episode.Views == 1 ? "vypočutie" : "vypočutí"}</p>
-                                        </div>
-                                        {episode.Audio && episode.Audio !== "" && episode.Audio !== null && (
-                                            <button
-                                                onClick={() => {
-                                                    selectEpisode(episode);
-                                                }}
-                                                className="flex-shrink-0 w-12 h-12 flex items-center justify-center rounded-full bg-[#d43c4a] hover:bg-[#b83744] transition-colors"
-                                                aria-label="Play episode"
-                                            >
-                                                <FontAwesomeIcon icon={faPlay} className="ml-1" />
-                                            </button>)}
-                                    </div>
-
-                                    <div className="mt-4">
-                                        <p className="text-justify">{episode.Description}</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        <Episode episode={episode} key={index} ShowName={ShowName} />
                     ))}
                 </div>
             </div>
