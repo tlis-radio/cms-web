ce568de65677426d5a977a8a0d29b4b3ea97bb3a
Tomáš Kordoš <kordo.t12@gmail.com>
2025-09-13 20:26:14 +0200

    Removed Members from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,7 @@ export default async function Home() {
       <h1 className="text-4xl text-white font-semibold mb-8 text-left ml-8"><span className="text-[#d43c4a] italic text-[1.4em] mr-2">TLIS</span> študentské rádio</h1>
       <Program />
       <ArchiveGrid />
-      <Members />
     </>
-    // archív
-    // novinky
-    // členovia
   );
 }
 
