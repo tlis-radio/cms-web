48e8fe8bb1c0065ea7922c6126a91f24f990533f
Tomáš Kordoš <kordo.t12@gmail.com>
2025-09-14 10:18:51 +0200

    banner websupport

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,12 @@ export default async function Home() {
     <>
       <h1 className="text-4xl text-white font-semibold mb-8 text-left ml-8"><span className="text-[#d43c4a] italic text-[1.4em] mr-2">TLIS</span> študentské rádio</h1>
       <Program />
+      <div className="flex justify-center w-full pt-8">
+        <a href="https://www.websupport.sk/" target="_blank" rel="noopener noreferrer">
+        <img src="/images/websupport-banner-large.png" className="px-4 hidden md:block" alt="Sponzor domény" />
+        <img src="/images/websupport-banner-small.png" className="px-4 visible md:hidden" alt="Sponzor domény" />
+        </a>
+      </div>
       <ArchiveGrid />
     </>
   );
