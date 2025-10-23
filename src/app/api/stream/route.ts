4e1edfa103a65cb7b711b82eca90cd9f345874f1
Tomáš Kordoš <kordo.t12@gmail.com>
2025-10-23 12:59:22 +0200

    fix attempt: enforce no-store

--- src/app/api/stream/route.ts
+++ src/app/api/stream/route.ts
@@ -1,9 +1,11 @@
 import CmsApiService from "@/services/cms-api-service";
 
 export async function GET(request: Request) {
-    
     const currentStreamTitle = await CmsApiService.Stream.getCurrentStreamTitle();
-    if (currentStreamTitle) return new Response(currentStreamTitle);
-
-    return new Response(null);
+    const headers = new Headers({ "Cache-Control": "no-store" });
+    
+    if (currentStreamTitle) return new Response(currentStreamTitle, { status: 200, headers });
+    return new Response(null, { status: 204, headers });
 }
+
+export const dynamic = "force-dynamic";
