bd8dbede5af59aa0716b16cfd9cd79947bfed5e7
Tomáš Kordoš <kordo.t12@gmail.com>
2025-12-07 21:17:14 +0100

    404 fix

---
+++ src/app/not-found.tsx
@@ -0,0 +1,18 @@
+import Header from "@/components/header";
+import NotFound from "@/components/NotFound";
+
+import Footer from "@/components/footer";
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Page() {
+	return<div className={`flex flex-col transition-[padding] duration-300 ease-in-out ${inter.className} bg-acoustic-foam bg-fixed min-h-[100vh]`}>
+        <Header />
+        <div className='flex flex-row justify-center w-full pt-[125px] lg:pt-[150px] mb-16'>
+            <div className='pt-4 w-full text-center max-w-7xl' >
+		  		<NotFound />
+            </div>
+        </div>
+        <Footer />
+    </div>
+}
