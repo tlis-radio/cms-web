663825c539fa60981ba47eb1a2d1beea49d61096
Tomáš Kordoš <kordo.t12@gmail.com>
2025-12-07 21:28:55 +0100

    fix: build

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,16 +3,21 @@ import NotFound from "@/components/NotFound";
 
 import Footer from "@/components/footer";
 import { Inter } from "next/font/google";
+import { PlayerProvider } from "@/context/PlayerContext";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Page() {
-	return<div className={`flex flex-col transition-[padding] duration-300 ease-in-out ${inter.className} bg-acoustic-foam bg-fixed min-h-[100vh]`}>
+  return (
+    <div className={`flex flex-col transition-[padding] duration-300 ease-in-out ${inter.className} bg-acoustic-foam bg-fixed min-h-[100vh]`}>
+      <PlayerProvider>
         <Header />
-        <div className='flex flex-row justify-center w-full pt-[125px] lg:pt-[150px] mb-16'>
-            <div className='pt-4 w-full text-center max-w-7xl' >
-		  		<NotFound />
-            </div>
+        <div className="flex flex-row justify-center w-full pt-[125px] lg:pt-[150px] mb-16">
+          <div className="pt-4 w-full text-center max-w-7xl">
+            <NotFound />
+          </div>
         </div>
         <Footer />
+      </PlayerProvider>
     </div>
+  );
 }
